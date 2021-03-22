c6ebd6329bce0aec97ab2f84f39161136e011b18
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2021-03-22 14:56:47 +0300

    level I - ready

---
+++ src/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Vector;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String fileMessage = "01010100101010110"; //010 101 001 010 101 10
+		int lenBlock = 3;
+		
+		Haffman haffman = new Haffman(fileMessage, lenBlock);
+		
+		
+	}
+
+}
